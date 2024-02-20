45c6b7905c2722e9bf8bf309487d25c1db82be09
atul55tripathi <141387139+atul55tripathi@users.noreply.github.com>
2024-02-20 19:33:59 +0530

    Add files via upload
    the edit details is created, but not added to dashboard, I am still learning routing within pages

--- components/TopNavigationBar.js
+++ components/TopNavigationBar.js
@@ -1,36 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../css/TopNavigationBar.css'; // Adjust the path to your CSS file
 
-function TopNavigationBar({ patientName }) {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
-
-  const handleEditProfile = () => {
-    // Logic for handling edit profile
-    console.log('Edit Profile clicked');
-  };
-
-  const handleSignOut = () => {
-    // Logic for handling sign out
-    console.log('Sign Out clicked');
-  };
-
+function TopNavigationBar({ patientName, onEditProfile, onSignOut }) {
   return (
     <div className="top-navbar">
       <span>Welcome, {patientName}</span>
-      <div className="profile-dropdown">
-        <div className="profile" onClick={toggleDropdown}>
-          Profile
-        </div>
-        {showDropdown && (
-          <div className="dropdown-content">
-            <div onClick={handleEditProfile}>Edit Profile</div>
-            <div onClick={handleSignOut}>Sign Out</div>
-          </div>
-        )}
+      <div className="right-links">
+        <span className="edit-details-link" onClick={onEditProfile}>
+          Edit Details&nbsp;&nbsp;&nbsp;
+        </span>
+        <span className="signout-link" onClick={onSignOut}>
+          Sign Out
+        </span>
       </div>
     </div>
   );
