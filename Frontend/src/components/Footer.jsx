2de29e42c326557c2e030e879a443d53480f40f7
Somya Malgudi <Somya.Malgudi@iiitb.ac.in>
2024-02-23 22:45:36 +0530

    created about us and contact us

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -6,11 +6,12 @@ function Footer() {
     <footer className="footer-container">
       <div className="footer-left">
         <p>&copy; 2024 E-Consultation. All rights reserved.</p>
+        <p>Trademark E-Consultation™</p>
       </div>
       <div className="footer-right">
+      <a href="/">Home</a>
         <a href="/aboutus">About Us</a>
         <a href="/contactus">Contact Us</a>
-        <p>Trademark E-Consultation™</p>
       </div>
     </footer>
   );
