f2260c5b8ed093f0fba3fa47321635b8b3344d32
Akshansh247 <jainakshansh3@gmail.com>
2024-02-27 03:07:44 +0530

    fixed the zoom problem , improved upload schedule

---
+++ Frontend/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import DoctorDashboard from "./Pages/Ddashboard";
+
+import PreviousAppointments from "./Pages/Appointment";
+import UploadSchedule from "./Pages/uploadschedule";
+import SideNavbar from "./components/sidenavbar";
+import Chats from "./Pages/ chats";
+
+
+function App() {
+ 
+  return (
+  
+
+      <Router>
+
+        <Navbar/>
+        <SideNavbar/>
+    
+      <Routes>
+     
+        <Route exact path = "/ddashboard" element = {<DoctorDashboard/>}/>
+        <Route exact path = "/ddashboard/appointment" element = {<PreviousAppointments/>}/>
+        <Route exact path = "/ddashboard/upload"  element = {<UploadSchedule/>}/>
+        <Route exact path="/ddashboard/chats"     element = {<Chats/>}/>
+      </Routes>
+      
+        
+      </Router>
+    
+        
+
+  
+  );
+}
+
+export default App;
