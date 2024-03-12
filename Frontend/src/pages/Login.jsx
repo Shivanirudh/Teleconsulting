d318ef29fd704b628ee928c11e986306ae29da3a
Somya Malgudi <Somya.Malgudi@iiitb.ac.in>
2024-03-12 13:10:16 +0530

    Making Visual Changes to Landing Page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import logo from './../images/logo.jpeg';
 import './../css/login.css';
 import Footer from '../components/Footer.jsx';
 import Header from '../components/Header.jsx';
@@ -22,10 +21,6 @@ function Login() {
     <div className="main-container">
       <Header />
       <div className='main-content'>
-        <div className="logo-container">
-          <img src={logo} alt="Logo" className="logo" />
-          <h2>E-Consultation</h2>
-        </div>
         <div className="login-wrapper">
           <div className="login-container">
             <h2>Doctor Login</h2>
